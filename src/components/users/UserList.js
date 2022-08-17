ccb0696cee4b62a3215c3ca51d1fd12f941175b9
Griffin Moyer <moyerga@outlook.com>
2022-08-17 10:45:19 -0500

    list of user profiles displays for admins

--- src/components/users/UserList.js
+++ src/components/users/UserList.js
@@ -1,3 +1,46 @@
+import { useState, useEffect } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { getAllUsers } from "../../managers/UserManager"
+
 export const UserList = () => {
-    return <>USER LIST IS GONNA GO HERE</>
+
+    const [users, setUsers] = useState([])
+
+    useEffect(
+        () => {
+            getAllUsers().then(setUsers)
+        },
+        []
+    )
+
+    return <section className="section">
+        <div className="column">
+            <table className="table is-fullwidth">
+                <thead>
+                    <tr>
+                        <th>Users</th>
+                        <th>Full Name</th>
+                        <th>User Type</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map(user => (
+                            <tr key={user.id}>
+                                <td>{user.user.username}</td>
+                                <td>{user.user.first_name} {user.user.last_name}</td>
+                                <td>
+                                    {
+                                        (user.user.is_staff)
+                                            ? <>Admin</>
+                                            : <>Author</>
+                                    }
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
+    </section>
 }
