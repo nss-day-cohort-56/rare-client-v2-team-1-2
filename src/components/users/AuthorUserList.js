4c12298167cfd6c3e8db11bffe51f526aeb2d05d
Kiefer Luttrell <kieferluttrell@gmail.com>
2022-08-18 14:36:41 -0500

    subscription mangaer and subscribe buttons added

--- src/components/users/AuthorUserList.js
+++ src/components/users/AuthorUserList.js
@@ -1,13 +1,17 @@
 import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
+import { createSubscription, updateSubscription } from "../../managers/SubscriptionManager"
 import { getAllActiveUsers } from "../../managers/UserManager"
 
 export const AuthorUserList = () => {
     const [users, setUsers] = useState([])
 
+    const handleSub = () => {
+        getAllActiveUsers().then(setUsers)
+    }
     useEffect(
         () => {
-            getAllActiveUsers().then(setUsers)
+            handleSub()
         },
         []
     )
@@ -27,9 +31,30 @@ export const AuthorUserList = () => {
                             <tr key={user.id}>
                                 <td><Link to={`/users/${user.id}`}>{user.user.username}</Link></td>
                                 <td>{user.user.first_name} {user.user.last_name}</td>
-                            </tr>
-                        ))
-                    }
+                                <div className="subscription__container">
+                                    {
+                                        user.following ? <button id={user.id}
+                                            onClick={(evt) => {
+                                                evt.preventDefault()
+                                                updateSubscription(user.id)
+                                                .then(handleSub)
+                                            }}>
+                                                Unsubscribe
+                                            </button>
+                                            :
+                                            <button id={user.id}
+                                            onClick={(evt) =>{
+                                                evt.preventDefault()
+                                                createSubscription(user.id)
+                                                .then(handleSub)
+                                            }}>
+                                                Subscribe
+                                            </button>
+                                    }
+                                    </div>
+                                    </tr>
+                                    )
+            )}
                 </tbody>
             </table>
         </div>
